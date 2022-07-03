a567d4809a8fc97e647aa9d2232c658dbccc03a0
exodus31 <sarthak.nandre31@gmail.com>
2022-07-03 16:38:03 +0530

    images+ex student

--- ybapp/models.py
+++ ybapp/models.py
@@ -1,3 +1,21 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+
+class Year(models.Model):
+    year = models.CharField(max_length=4,null=False, blank=False)    
+
+    def __str__(self):
+        return self.year
+
+class Student(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    img = models.ImageField(blank=False, null=False)
+    age = models.IntegerField()
+    year = models.ForeignKey(Year, on_delete=models.SET_NULL,null=True)
+
+    def __str__(self):
+        return self.fname + ' ' + self.lname
