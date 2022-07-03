29396daa3760f2bc5ffb5074b08a051998ea00fe
exodus31 <sarthak.nandre31@gmail.com>
2022-07-03 22:54:01 +0530

    models

--- ybapp/admin.py
+++ ybapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Year, Student
+from .models import Year, Student, TextMemories
 
 # Register your models here.
 
 admin.site.register(Year)
 admin.site.register(Student)
+admin.site.register(TextMemories)
