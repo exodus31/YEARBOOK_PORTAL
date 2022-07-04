f139c2f619b082a706b6ee4e8b8cb285a4c8419e
Ghost4681 <64606908+Ghost4681@users.noreply.github.com>
2022-07-04 14:57:21 +0530

    Changed homepage

--- ybapp/admin.py
+++ ybapp/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Year, Student, TextMemories
+from .models import Student
 
 # Register your models here.
 
-admin.site.register(Year)
+# admin.site.register(Year)
 admin.site.register(Student)
-admin.site.register(TextMemories)
+# admin.site.register(TextMemories)
